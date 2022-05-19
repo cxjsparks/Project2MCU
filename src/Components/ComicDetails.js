204a224920528006ecc70c05b7b50f4ec3be7e54
CHLOE JAGER <chloejager@chloes-mbp.attlocal.net>
2022-05-19 16:54:25 -0700

    Filter working now thanks to Marc Nellie and Stack

--- src/Components/ComicDetails.js
+++ src/Components/ComicDetails.js
@@ -1,38 +1,28 @@
 import { useEffect, useState } from "react";
+import {useParams} from "react-router";
+import Comics from "./Comics";
 
 function ComicDetails(props) {
     const publicKey = "30d1009ef6cc717088bed0009f9ee355"
     const hash = "37efc00f48c455559cea8f56cf80b20d"
     const [comic, setComic] = useState({});
-
-// console.log(props.comic)
-
-  useEffect(() => {
-    fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publicKey}&hash=${hash}${props.comic}`)
-      .then((response) => response.json())
-      .then((data) => {
-          const chloes=props.comic.map((chloe) => {
-              console.log(chloe)
-              return chloe;
-          }) 
-          for (let i=0; i<chloes.length; i++) {
-              if (chloes[i]) {
-                  console.log(chloes[i])
-              }
-          }
-        // console.log(data)
-        setComic(data);
-      });
-  }, [props.comic]);
+    let params = useParams();
+    console.log(params)
+    
+    console.log(props.comic)
+    const result = props.comic.filter(comic => {
+        console.log(params.id)
+        return params.id == comic.id});
+    console.log(result)
 
 
   let comicDetailHtml = "";
-  if (comic.name) {
+  if (result[0]) {
     comicDetailHtml = (
       <div>
-        <h3>{comic.name}</h3>
+        <h3>{result[0].title}</h3>
         {/* <img src={data.data.results.title.thumbnail.extension} */}
-        <img src={"https://i.annihil.us/u/prod/marvel/i/mg/2/f0/4bc6670c80007/portrait_incredible.jpg"} /> */}
+        <img src={`${result[0].thumbnail.path}.${result[0].thumbnail.extension}`} />
         <button onClick={() => props.addComic(comic)}></button>
       </div>
     );
@@ -49,3 +39,27 @@ function ComicDetails(props) {
 export default ComicDetails;
 
   {/* <img src={"https://i.annihil.us/u/prod/marvel/i/mg/2/f0/4bc6670c80007/portrait_incredible.jpg"} /> */}
+
+  // console.log(props.comic)
+// console.log(props.comic)
+//   useEffect(() => {
+//     fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publicKey}&hash=${hash}${props.comic}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+          
+//         //   const chloes=props.comic.map((chloe) => {
+//         //       console.log(chloe)
+//         //       return chloe;
+//         //   }) 
+//         //   for (let i=0; i<chloes.length; i++) {
+//         //       if (chloes[i]) {
+//         //           console.log(chloes[i])
+//         //       }
+//         //   }
+//         console.log(data)
+//         setComic(data);
+
+//         const result = comic.filter(comic => params === comic.id);
+
+//       });
+//   }, []);
