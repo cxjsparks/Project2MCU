e5266eb938a1ac21dbf7ee923bdb0e04a0f3d945
CHLOE JAGER <chloejager@chloes-mbp.attlocal.net>
2022-05-19 19:42:40 -0700

    Updated wording and styiling on home page, and relocated attribute to bottom right-hand corner.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const Home = () => {
     return (
-        <div>
+        <div className="stan">
             <h1>Welcome to the MCU, from the brilliant mind of Stan Lee!</h1>
-            <h3>You've enjoyed watching the action, the thrills, the friendships, the fights.</h3>
-            <h3>You've laughed, you've cried, you've cheered.</h3>
-            <h2>Now get to know Thor just a little bit better.</h2>
+            <h2>I admit, I'm much more into the movies than the comic books.</h2>
+            <h2>But the artwork is really stunning!</h2>
+            <h2>Come take a look at a small sample of what comic books can be.</h2>
         </div>
     )  
      
