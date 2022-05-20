68ab53365e98a81f073619f0aee2354c6c9b64f0
CHLOE JAGER <chloejager@chloes-mbp.attlocal.net>
2022-05-19 22:37:48 -0700

    Minor change to home page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
             <h1>Welcome to Marvel, from the brilliant mind of Stan Lee!</h1>
             <h2>I admit, I'm much more into the movies than the comic books.</h2>
             <h2>But the artwork is really stunning!</h2>
-            <h2>Come take a look at a small sample of what comic books can be.</h2>
+            <h2>Come take a look at a sample of what comic books can be.</h2>
         </div>
     )  
      
